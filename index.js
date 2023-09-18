61cdb51e06e4d89da600e0f39a1917e1369f55f1
Andrey Emelianov <621077@mail.ru>
2023-09-18 17:37:44 +0300

    реализовал вспомогательную функцию которая проверяет наличие токена и расшифровывает его и реализовал get метод получения информации об авторизованном пользователе

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import bcrypt from 'bcrypt';
 
 import { registerValidation } from './validations/auth.js';
 import { validationResult } from 'express-validator';
+
 import UserModel from './models/User.js';
+import checkAuth from './utils/checkAuth.js';
 
 config();
 
@@ -102,6 +104,27 @@ app.post('/auth/register', registerValidation, async (req, res) => {
     console.log(err);
     res.status(500).json({
       message: 'Не удалось зарегистрироваться',
+    });
+  }
+});
+
+app.get('/auth/me', checkAuth, async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден',
+      });
+    }
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json(userData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Нет доступа',
     });
   }
 });
