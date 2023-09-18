c244dfc7ce1bdf7a8431421c7a6eab6d42685413
Andrey Emelianov <621077@mail.ru>
2023-09-18 15:51:24 +0300

    подключил bcrypt, захешировал приходящие пароли при регистрации, сделал сохранение информации о зарегистрированном пользователе в db

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 import { registerValidation } from './validations/auth.js';
 import { validationResult } from 'express-validator';
+import UserModel from './models/User.js';
 
 mongoose
-  .connect('mongodb+srv://640775:wwwwww@cluster0.3l5fedz.mongodb.net/?retryWrites=true&w=majority')
+  .connect(
+    'mongodb+srv://640775:wwwwww@cluster0.3l5fedz.mongodb.net/blog?retryWrites=true&w=majority',
+  )
   .then(() => console.log('DB start'))
   .catch((err) => console.log('DB error', err));
 
@@ -14,16 +18,34 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
-  const errors = validationResult(req);
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось зарегистрироваться',
+    });
   }
-
-  res.json({
-    success: true,
-  });
 });
 
 app.listen(4444, (err) => {
