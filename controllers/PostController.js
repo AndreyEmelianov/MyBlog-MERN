a801682f6102ad2995c1098fe340a11e47f9f247
Andrey Emelianov <621077@mail.ru>
2023-09-18 19:35:23 +0300

    реализовал получение всех статей и удаление одной статьи

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -40,6 +40,29 @@ export const getOne = async (req, res) => {
   }
 };
 
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const doc = await PostModel.findOneAndDelete({ _id: postId });
+
+    if (!doc) {
+      return res.status(404).json({
+        message: 'Статья не найдена',
+      });
+    }
+
+    res.json({
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Ошибка получения статей',
+    });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
