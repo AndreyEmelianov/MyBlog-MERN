9917256dff8e1c766a30ed7b188a2d69dbe7e09f
Andrey Emelianov <621077@mail.ru>
2023-09-18 15:33:38 +0300

    подключил  express-validator, добавил валидацию для данных пользователя

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 
+import { registerValidation } from './validations/auth.js';
+import { validationResult } from 'express-validator';
+
 mongoose
   .connect('mongodb+srv://640775:wwwwww@cluster0.3l5fedz.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log('DB start'))
@@ -11,7 +14,17 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', (req, res) => {});
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
+  res.json({
+    success: true,
+  });
+});
 
 app.listen(4444, (err) => {
   if (err) {
