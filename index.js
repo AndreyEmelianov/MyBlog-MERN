3fb74e1e805886be85f8e7962ab89b95cbbaecee
Andrey Emelianov <621077@mail.ru>
2023-09-18 15:19:59 +0300

    создал модель пользователя

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 
 mongoose
-  .connect(
-    'mongodb+srv://640775:qunbvqsw@cluster0.3l5fedz.mongodb.net/?retryWrites=true&w=majority',
-  )
+  .connect('mongodb+srv://640775:wwwwww@cluster0.3l5fedz.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log('DB start'))
   .catch((err) => console.log('DB error', err));
 
@@ -13,24 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
-
-app.post('/auth/login', (req, res) => {
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: 'Андрей Емельянов',
-    },
-    'secret123',
-  );
-
-  res.json({
-    success: true,
-    token,
-  });
-});
+app.post('/auth/register', (req, res) => {});
 
 app.listen(4444, (err) => {
   if (err) {
